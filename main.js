9bd5b42152fc0f51c9f3ca2855c67fe87b4c5853
James Horton <jameshorton@gmail.com>
2018-08-28 15:49:04 -0400

    so many CSS issues at the moment

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const playerIcon = document.createElement('div')
 playerIcon.id = 'playerIcon';
-let playerLocation = [0,9];
+let playerLocation = [0, 9];
 const rowsWrapper = document.getElementById("rowsWrapper");
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
@@ -21,43 +21,39 @@ const map = [
 ];
 
 function createRows() {
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < map.length; i++) {
         let row = document.createElement('div');
         row.className = 'row';
-        row.style.height = '28px';
-        row.style.width = '588px';
-        row.id = 'row' + i
+        // row.style.height = '28px';
+        // row.style.width = '588px';
+        row.id = 'row' + i;
+        row.dataset.rowNumber = i;
         createCells(row);
         rowsWrapper.appendChild(row)
-        // console.log(row);
     }
 }
 
 function createCells(row) {
-    for (let i = 0; i < 21; i++) {
+    for (let i = 0; i < map[0].length; i++) {
         let cell = document.createElement('div');
         cell.className = 'cell';
         cell.style.height = '28px';
         cell.style.width = '28px';
         cell.id = row.id + "cell" + i;
-        // cell.dataset.rowNumber = row.dataset.rowNumber
+        map[row.dataset.rowNumber][i] == 'W' ?
+            cell.className = 'wall' :
+            cell.className = 'floor'
         row.appendChild(cell);
     }
-}
-
-function createPlayerIcon(cellNumber, rowNumber) {
-    const playerIcon = document.createElement('div')
-    playerIcon.id = 'playerIcon';
-    let playerLocation = document.getElementById('row' + rowNumber + 'cell' + cellNumber)
-    playerLocation.appendChild(playerIcon);
-
 }
 
 function changePlayerLocation(Right, Down) {
     let rowNumber = playerLocation[1] + Down;
     let cellNumber = playerLocation[0] + Right;
     let mapLocation = (map[rowNumber][cellNumber]);
+    let direction = whatDirection(Right, Down);
     if (mapLocation !== 'W') {
+        playerIcon.style.animationName = "slide" + direction
         let playerLocationDiv = document.getElementById('row' + rowNumber + 'cell' + cellNumber)
         playerLocationDiv.appendChild(playerIcon);
         playerLocation = [cellNumber, rowNumber];
@@ -66,7 +62,25 @@ function changePlayerLocation(Right, Down) {
         youWon();
     }
 }
-function youWon(){
+
+function whatDirection (Right, Down){
+    let direction = '';
+    if (Right == 1){
+        direction = 'Right';
+    }
+    if (Right == -1){
+        direction = 'Left';
+    }
+    if (Down == 1){
+        direction = 'Down';
+    }
+    if (Down == -1){
+        direction = 'Up';
+    }
+    return direction;
+}
+
+function youWon() {
     let text = document.createTextNode("You won!");
     let element = document.createElement('div');
     element.appendChild(text);
@@ -78,20 +92,22 @@ function youWon(){
 createRows();
 changePlayerLocation(0, 0); //game init: sets the playerIcon and mapLocation to the start position
 
-
-
-let arrowKeyPressed = function(event){
+let arrowKeyPressed = function (event) {
     if (event.key == 'ArrowRight') {
-        changePlayerLocation(1,0);
+        // playerIcon.style.animationName = "slideRight";
+        changePlayerLocation(1, 0);
     }
     if (event.key == 'ArrowLeft') {
-        changePlayerLocation(-1,0);
+        // playerIcon.style.animationName = "slideLeft";
+        changePlayerLocation(-1, 0);
     }
     if (event.key == 'ArrowDown') {
-        changePlayerLocation(0,1);
+        // playerIcon.style.animationName = "slideDown";
+        changePlayerLocation(0, 1);
     }
     if (event.key == 'ArrowUp') {
-        changePlayerLocation(0,-1);
+        // playerIcon.style.animationName = "slideUp";
+        changePlayerLocation(0, -1);
     }
 }
 
